b4ea0e18f038e5a4dc64c5affeebd1ee9c7d35dd
Alex Padron <alexander.f.padron@gmail.com>
2020-05-01 08:02:07 -0400

    Update g.sh

--- g.sh
+++ g.sh
@@ -131,3 +131,10 @@ alias cm="docker-compose"
 
 alias l="ls -la"
 alias c="clear"
+
+##############################
+###### Docker aliases ########
+##############################
+
+# Start a local postgres instance in Docker
+alias start_postgres="docker run -p 5432:5432 --name some-postgres -e POSTGRES_PASSWORD=password -d postgres"
